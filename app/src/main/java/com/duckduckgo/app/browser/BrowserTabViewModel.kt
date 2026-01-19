1e974b8aef083a7067ac316c95cc188c42933ec4
David González <malmstein@gmail.com>
2026-01-19 22:27:38 +0100

    expand sheet on text input

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
@@ -4474,7 +4474,11 @@ class BrowserTabViewModel @Inject constructor(
 
         when {
             duckAiFeatureState.showContextualMode.value && !isNtp -> {
-                command.value = Command.ShowDuckAIContextualMode
+                val contextualUrl = url
+                val contextualTitle = title
+                if (contextualUrl != null && contextualTitle != null) {
+                    command.value = Command.ShowDuckAIContextualMode(contextualUrl, contextualTitle)
+                }
             }
 
             duckAiFeatureState.showFullScreenMode.value -> {
